b6c39e0c1314902754f3b7cb8d4c6aab6070d1b5
Nikitulka0120 <107490259+Nikitulka0120@users.noreply.github.com>
2024-10-24 16:34:53 +0700

    Задание 8.py

--- Лабараторная 6/Задание 8.py
+++ Лабараторная 6/Задание 8.py
@@ -1,7 +1,8 @@
 dec=0
 step=0
-bi=input("Введите двоичное число")
-for i in range(len(bi)-1),0,-1):
+bi=input("Введите двоичное число: ")
+for i in range((len(bi)-1),-1,-1):
     if (bi[i]=="1"):
-        dec=dec+=2**step
+        dec=dec+2**step
     step+=1
+print(dec)
